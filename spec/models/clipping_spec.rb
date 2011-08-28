357f8732d0883a48806c33548adfb2bf14b3b9a1
Mark <pagetribemail@gmail.com>
2011-08-28 20:49:26 +1000

    some work on specs and associations

--- spec/models/clipping_spec.rb
+++ spec/models/clipping_spec.rb
@@ -1,13 +1,33 @@
 require 'spec_helper'
 
 describe Clipping do
-  describe "Associations" do
-    it 'belongs to notebook' do
-      Clipping.new.should respond_to(:notebook)
+    
+  before(:each) do
+    @nb = Factory(:notebook)
+  end
+
+  describe "notebook associations" do
+  
+    before(:each) do
+      @clipping = @nb.clippings.create()
     end
-    it 'can retrieve a notebook' do
-      c = Factory(:clipping)
-      c.notebook.should be_kind_of(Notebook)
+    
+    it "should have a notebook attribute" do
+      @clipping.should respond_to(:notebook)
     end
+    
+    it "should have the right associated notebook" do
+      @clipping.notebook_id.should == @nb.id
+      @clipping.notebook.should == @nb
+    end
+    
+    
+    # it 'belongs to notebook' do
+    #   Clipping.new.should respond_to(:notebook)
+    # end
+    # it 'can retrieve a notebook' do
+    #   c = Factory(:clipping)
+    #   c.notebook.should be_kind_of(Notebook)
+    # end
   end
 end
--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe Notebook do
   describe "Associations" do
-    it "has many clippings" do
-      Notebook.new.should respond_to(:clippings)
+    before(:each) do
+      @nb = Factory(:notebook)
     end
-    it "can retrieve clippings" do
-      n = Factory(:notebook)
-      n.clippings.count.should_not eq 0 
+    it "should have a clippings attribute" do
+      @nb.should respond_to(:clippings)
     end
+    # it "has many clippings" do
+    #   Notebook.new.should respond_to(:clippings)
+    # end
+    # it "can retrieve clippings" do
+    #   n = Factory(:notebook)
+    #   n.clippings.count.should_not eq 0 
+    # end
   end
 end
